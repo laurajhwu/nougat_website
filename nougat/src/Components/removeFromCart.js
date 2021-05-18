d635f810e1935ce1b828850ee48db1ffa9533a61
Laura Wu <laura.jh.wu@gmail.com>
2021-05-18 16:14:56 +0800

    create delete remove component

---
+++ nougat/src/Components/removeFromCart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import Api from "../utils/Api";
+import { DeleteOutline } from "@styled-icons/typicons";
+import { useDispatch } from "react-redux";
+import { updateMember } from "../redux/actions/member";
+
+const DeleteIcon = styled(DeleteOutline)`
+  width: 25px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const CartButton = styled.button``;
+
+function Delete(props) {
+  const dispatch = useDispatch();
+
+  function deleteOnClick() {
+    props.member.cart_items = props.member.cart_items.filter(
+      (item) => item.id !== props.productId
+    );
+    Api.updateCartItems(props.member);
+    alert("已從購物車移除");
+    dispatch(updateMember(props.member));
+  }
+
+  return <DeleteIcon onClick={deleteOnClick}></DeleteIcon>;
+}
+
+export default Delete;
