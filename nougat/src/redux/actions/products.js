f537841708c2952dba108bfaa82ed930035461b2
Laura Wu <laura.jh.wu@gmail.com>
2021-05-21 15:10:52 +0800

    update redux to update specific products

--- nougat/src/redux/actions/products.js
+++ nougat/src/redux/actions/products.js
@@ -2,3 +2,8 @@ export const getProductsData = (products) => ({
   type: "GET_PRODUCTS",
   payload: products,
 });
+
+export const updateProduct = (productId, prop, data) => ({
+  type: "UPDATE_PRODUCT",
+  payload: { id: productId, prop, data },
+});
--- nougat/src/redux/reducers/products.js
+++ nougat/src/redux/reducers/products.js
@@ -1,7 +1,14 @@
+import convertToObject from "../../utils/arrayToObjectConverter";
+
 function products(state = [], action) {
   switch (action.type) {
     case "GET_PRODUCTS":
       return action.payload;
+    case "UPDATE_PRODUCT":
+      const args = action.payload;
+      const productObj = convertToObject(state, "id");
+      productObj[args.id][args.prop] = args.data;
+      return [...state];
     default:
       return state;
   }
