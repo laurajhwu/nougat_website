038535c13a7b47b1f7f3930cb83198984eb71269
Laura Wu <laura.jh.wu@gmail.com>
2021-06-20 15:39:08 +0800

    create testing for getTimeRange util

--- nougat/src/utils/getTimeRange.js
+++ nougat/src/utils/getTimeRange.js
@@ -1,15 +1,17 @@
 import { addMinutes } from "date-fns";
 import { stringTime } from "./dateTimeFormat";
 
+function getTimeSum(time) {
+  const splitTime = stringTime(time).split(":");
+  return +splitTime[0] + +splitTime[1] / 60;
+}
+
 export default function getRange(stringDate, timeSettings) {
   const range = [];
-  const endingTime = addMinutes(
-    new Date(`${stringDate} ${timeSettings.end_time}`),
-    timeSettings.interval
-  );
+  const endingTime = new Date(`${stringDate} ${timeSettings.end_time}`);
   let time = new Date(`${stringDate} ${timeSettings.start_time}`);
 
-  while (stringTime(time) !== stringTime(endingTime)) {
+  while (getTimeSum(time) <= getTimeSum(endingTime)) {
     range.push(time);
     time = addMinutes(time, timeSettings.interval);
   }
