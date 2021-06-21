918ba6d1b96dfe861cc2343f2a226676b2c78c8a
Laura Wu <laura.jh.wu@gmail.com>
2021-06-21 12:43:15 +0800

    refactor products inventory

--- nougat/src/Admin/MainPage/MainContent/Inventory/Products/Edit/AddProduct.js
+++ nougat/src/Admin/MainPage/MainContent/Inventory/Products/Edit/AddProduct.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Modal, Button, Form, Col } from "react-bootstrap";
-import IngredientSection from "../IngredientSection";
-import Api from "../../../../../../utils/Api";
+import { Modal } from "react-bootstrap";
 import propTypes from "prop-types";
-
-import { Img, File } from "./styles";
+import Api from "../../../../../../utils/Api";
+import FormBase from "./FormBase";
 
 export default function Edit(props) {
   const { handleClose, show } = props;
@@ -23,32 +21,7 @@ export default function Edit(props) {
     handleClose();
   }
 
-  function getEditData(event, prop) {
-    const rawValue = event.target.value.trim();
-    const value = isNaN(Number(rawValue)) ? rawValue : Number(rawValue);
-
-    changes[prop] = value;
-  }
-
-  function handleUploadImage(event) {
-    if (event.target.files[0]) {
-      Api.getImageUrl("product_image", event.target.files[0])
-        .then((url) => {
-          changes.image = url;
-          setImage(url);
-        })
-        .catch((error) => {
-          setImage(null);
-          delete changes.image;
-          throw error;
-        });
-    } else {
-      setImage(null);
-      delete changes.image;
-    }
-  }
-
-  function handOnSubmit(event) {
+  function handleOnSubmit(event) {
     event.preventDefault();
     setChanges({
       ...changes,
@@ -101,43 +74,6 @@ export default function Edit(props) {
       });
   }
 
-  function submitEdit() {
-    const valid = Object.values(invalid).every((input) => {
-      if (typeof input === "object") {
-        return Object.values(input).every((amount) => amount === false);
-      } else {
-        return input === false;
-      }
-    });
-
-    if (Object.keys(changes).length !== 0 && valid) {
-      if (changes.name) {
-        Api.checkSameProduct(changes.name).then((isValid) => {
-          if (isValid) {
-            postEdit();
-          } else {
-            changes.name = "";
-            validateChanges();
-          }
-        });
-      } else {
-        postEdit();
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (Object.keys(changes).length !== 0) {
-      validateChanges();
-    }
-  }, [changes]);
-
-  useEffect(() => {
-    if (Object.keys(invalid).length !== 0) {
-      submitEdit();
-    }
-  }, [invalid]);
-
   return (
     <Modal
       show={show}
@@ -148,96 +84,23 @@ export default function Edit(props) {
       <Modal.Header closeButton>
         <Modal.Title>新增產品</Modal.Title>
       </Modal.Header>
-      <Form onSubmit={handOnSubmit}>
-        <Modal.Body>
-          <Form.Group>
-            <Form.Label>產品名稱</Form.Label>
-            <Form.Control
-              onChange={(event) => getEditData(event, "name")}
-              isInvalid={invalid.name}
-            />
-            <Form.Control.Feedback type="invalid">
-              報錯：名稱已被使用過或未填
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Row>
-            <Form.Group as={Col}>
-              <Form.Label>售價</Form.Label>
-              <Form.Control
-                onChange={(event) => getEditData(event, "price")}
-                isInvalid={invalid.price}
-              />
-              <Form.Control.Feedback type="invalid">
-                售價為必填數字
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group as={Col}>
-              <Form.Label>剩餘庫存</Form.Label>
-              <Form.Control
-                onChange={(event) => getEditData(event, "stock")}
-                isInvalid={invalid.stock}
-              />
-              <Form.Control.Feedback type="invalid">
-                庫存為必填數字
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group as={Col}>
-              <Form.Label>單位</Form.Label>
-              <Form.Control
-                placeholder="以出售單位為主"
-                onChange={(event) => getEditData(event, "unit")}
-                isInvalid={invalid.unit}
-              />
-              <Form.Control.Feedback type="invalid">
-                請填入單位
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Form.Row>
-          <div className="mb-3">
-            <File id="formcheck-api-custom" custom>
-              <Form.File.Input
-                accept="image/*,.pdf"
-                isValid={!!image}
-                isInvalid={invalid.image}
-                onChange={handleUploadImage}
-              />
-              <Form.File.Label data-browse="上傳產品圖">
-                {image || ""}
-              </Form.File.Label>
-              <Img src={image || ""} />
-              <Form.Control.Feedback type="valid">
-                上傳成功
-              </Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid">
-                請上傳圖片
-              </Form.Control.Feedback>
-            </File>
-          </div>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label>產品簡述</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              placeholder="成分、注意事項等"
-              onChange={(event) => getEditData(event, "description")}
-            />
-          </Form.Group>
-
-          <IngredientSection
-            prodIngredient={prodIngredient}
-            setProdIngredient={setProdIngredient}
-            getEditData={getEditData}
-            invalid={invalid.ingredients}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="warning" type="submit">
-            新增
-          </Button>
-        </Modal.Footer>
-      </Form>
+      <FormBase
+        {...{
+          handleOnSubmit,
+          changes,
+          invalid,
+          image,
+          setImage,
+          validateChanges,
+          ingredientProps: {
+            prodIngredient,
+            setProdIngredient,
+          },
+          postEdit,
+          buttonProps: { variant: "warning" },
+          buttonLabel: "新增",
+        }}
+      />
     </Modal>
   );
 }
